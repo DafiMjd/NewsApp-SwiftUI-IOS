75ba9d6ab5c48d83663c4e711a50e22549f5fa55
Dafi Majid F <dafi.majid@megafinance.co.id>
2022-11-02 08:54:52 +0700

    Initial Commit

---
+++ NewsApp-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NewsApp-SwiftUI
+//
+//  Created by MF_Dafi Majid Fadhlih on 11/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
